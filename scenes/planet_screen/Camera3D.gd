7e4b658490f37ba849a0e5bc14618354ea9835fc
PastThePixels <pastthescreen@protonmail.com>
2023-11-14 22:52:56 -0700

    Improve mouse navigation

--- scenes/planet_screen/Camera3D.gd
+++ scenes/planet_screen/Camera3D.gd
@@ -1,8 +1,10 @@
 extends Camera3D
 
-@export var speed : float = 1;
+@export var speed : float = 50;
 
 @export var movement_threshold = 0.2;
+
+var movement_velocity = Vector3()
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,13 +14,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	movement_velocity = movement_velocity.cubic_interpolate(Vector3(), movement_velocity, Vector3(), 0.2)
+	position += movement_velocity
 	# Mouse panning
 	var mouse_pos_normalized = Vector2(get_viewport().get_mouse_position()) / Vector2(get_viewport().size)
-	if mouse_pos_normalized.y > (1 - movement_threshold):
-		self.position.z += speed * (mouse_pos_normalized.y - (1 - movement_threshold)) * delta
-	if mouse_pos_normalized.y < (movement_threshold):
-		self.position.z += speed * (mouse_pos_normalized.y - movement_threshold) * delta
-	if mouse_pos_normalized.x > (1 - movement_threshold):
-		self.position.x += speed * (mouse_pos_normalized.x - (1 - movement_threshold)) * delta
-	if mouse_pos_normalized.x < (movement_threshold):
-		self.position.x += speed * (mouse_pos_normalized.x - movement_threshold) * delta
+	# If mouse_pos_normalised is on one of the edges of the screen
+	if mouse_pos_normalized.y > (1 - movement_threshold) or \
+		mouse_pos_normalized.y < (movement_threshold) or \
+		mouse_pos_normalized.x > (1 - movement_threshold) or \
+		mouse_pos_normalized.x < (movement_threshold):
+		var amount = (mouse_pos_normalized - Vector2(0.5, 0.5)) * speed * delta
+		movement_velocity.z = amount.y
+		movement_velocity.x = amount.x
