c3c6df1501683484391e60245d20469f593a9fa7
Zirconova <zirconova@gmail.com>
2023-11-16 00:07:21 -0700

    Organized menu and added background
    Grouped buttons in a VBox, added quit game button, added title, added background shader.

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/planet_screen/planet_screen.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
