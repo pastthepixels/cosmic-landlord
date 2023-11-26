a1f37f0f51d6e136430ce475058a2297ee842c06
PastThePixels <pastthescreen@protonmail.com>
2023-11-26 13:04:07 -0700

    Redesign machine purchase screen

--- scenes/machines/m_p_s_label.gd
+++ scenes/machines/m_p_s_label.gd
@@ -10,18 +10,17 @@ func set_stats(machine):
 	self.machine = machine
 	$%Name.text = machine.name
 	# Cost/wait time
-	$%CostPerRun/Value.text = "$%d" % machine.delta_cost
-	$%CooldownTime/Value.text = str(machine.get_node("Timer").wait_time)
+	$%CostPerRun.text = "$%d/cycle" % machine.delta_cost
 	$VBoxContainer/Purchase.text = "Purchase ($%d)" % machine.upfront_cost
 	# Changes in things
-	$%delta_temperature_level/Value.text = "%.2f" % (machine.delta_temperature_level * 100) + "%"
-	$%delta_temperature_level.visible = machine.delta_temperature_level != 0
-	$%delta_oxygen_level/Value.text = "%.2f" % (machine.delta_oxygen_level * 100) + "%"
-	$%delta_oxygen_level.visible = machine.delta_oxygen_level != 0
-	$%delta_carbon_dioxide_level/Value.text = "%.2f" % (machine.delta_carbon_dioxide_level * 100) + "%"
-	$%delta_carbon_dioxide_level.visible = machine.delta_carbon_dioxide_level != 0
-	$%delta_water_to_land_ratio/Value.text = "%.2f" % (machine.delta_water_to_land_ratio * 100) + "%"
-	$%delta_water_to_land_ratio.visible = machine.delta_water_to_land_ratio != 0
+	$%Temperature.text = ("+" if machine.delta_temperature_level > 0 else "-") + " Temperature"
+	$%Temperature.visible = machine.delta_temperature_level != 0
+	$%Oxygen.text = ("+" if machine.delta_oxygen_level > 0 else "-") + " Oxygen"
+	$%Oxygen.visible = machine.delta_oxygen_level != 0
+	$%CarbonDioxide.text = ("+" if machine.delta_carbon_dioxide_level > 0 else "-") + " Carbon Dioxide"
+	$%CarbonDioxide.visible = machine.delta_carbon_dioxide_level != 0
+	$%Water.text = ("+" if machine.delta_water_to_land_ratio > 0 else "-") + " Water"
+	$%Water.visible = machine.delta_water_to_land_ratio != 0
 
 func set_purchasable():
 	$VBoxContainer/Destroy.hide()
